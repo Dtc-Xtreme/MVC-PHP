bc1533eea6f7c5755f76887d95398719bb00fcdb
Dtc-Xtreme <dtc-xtreme@hotmail.com>
2023-05-28 17:14:49 +0200

    settings

---
+++ MVCDTC/Settings.php
@@ -0,0 +1,24 @@
+<?php
+namespace MVCDTC;
+
+class Settings{
+    const MVC_PATH = "../MVCDTC/";
+    const ROOT_PATH = "../src/"; 
+    const LAYOUT_PATH = "Views/Layout.php";
+    const CONTROLLERS_PATH = "Controllers";
+    const VIEWS_PATH = "Views";
+    const MODELS_PATH = "Models/";
+
+    const PUBLIC_PATH = "/var/www/project/public";
+
+    const MYSQLI_SERVER_NAME = "";
+    const MYSQLI_USER_NAME = "";
+    const MYSQLI_PASSWORD = "";
+    const MYSQLI_DB_NAME = "";
+
+    const LOGFILESPATH = "/var/www/project/log/";
+    const NAMESPACE_BASE = 'MVCDTC';
+    const NAMESPACE_PROJECT = 'MVCProject';
+}
+
+?>
